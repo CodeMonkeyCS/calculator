26021bd6c2a9a3afb3e6320f0327d0b2fdc49db7
Cameron Small <csmall@aegistg.com>
2024-02-21 18:04:02 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Cameron Small on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
